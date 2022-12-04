22452e4f4616616525c8e82e8e378f79276d3998
Kulakoff <86190227+UladzimirKulakou@users.noreply.github.com>
2022-12-05 01:09:46 +0300

    created elements for main View

--- SlmaxTestovoeZadanie/SlmaxTestovoeZadanie/MainScreen.swift
+++ SlmaxTestovoeZadanie/SlmaxTestovoeZadanie/MainScreen.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct MainScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            TopScreenImage()
+            Task()
+            Task()
+            Spacer()
+            CreateNewTask()
+        }
+        .padding(.bottom, 40)
+        .ignoresSafeArea()
+        
     }
 }
 
